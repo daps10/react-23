8ca2e769d51feb5ad6628eaa04bd4992b565a240
daps10 <darpanchavhan75@gmail.com>
2023-08-29 20:39:06 +0530

    Feat: adding complex component

--- Starting-setup/src/components/ExpenseItem.js
+++ Starting-setup/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-  return <h2>Expense Item!</h2>
+  return (
+    <div>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <h2>Amount</h2>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
