27bbf0723be06c593d6392a1935bcee62e4e4aff
daps10 <darpanchavhan75@gmail.com>
2023-10-04 20:15:23 +0530

    Feat: Using stateful lists
    - Used the dummy expenses array as the initial expenses.
    - Then used the dynamic expense with the help of the addExpenseHandler function which will returns the expense.
    - That expense will be adding into the existing expense array.

--- Starting-setup/src/App.js
+++ Starting-setup/src/App.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  {
+    title: 'Car Insurance',
+    amount: 298,
+    date: new Date()
+  }
+]
+
 const App = () => {
-  const expenses = [
-    {
-      title: 'Car Insurance',
-      amount: 298,
-      date: new Date()
-    }
-  ]
+
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log('in app.js');
-    console.log(expense);
+    // used spread to update state.
+    // setExpenses([expense, ...expenses])
+
+    // used prevexpenses which would return recent array
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
   }
 
   return (
