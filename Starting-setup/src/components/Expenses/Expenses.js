e7e1d3044305cd79c30b8357147b9e1a51d97cf5
daps10 <darpanchavhan75@gmail.com>
2023-10-04 18:20:40 +0530

    Feat: Assignments
    - Created new component for expenseFilter.
    - Which shall gives the years and aligned with pass the year up directory.
    - In expenses component get the selected filter year and stored it into state.

--- Starting-setup/src/components/Expenses/Expenses.js
+++ Starting-setup/src/components/Expenses/Expenses.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+  
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
+
   return (
-    <Card className="expenses">
-      <ExpenseItem 
-            title={props.expenses[0].title} 
-            date={props.expenses[0].date}
-            amount={props.expenses[0].amount}
-            />
-    </Card>
+    <div>
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      <Card className="expenses">
+        <ExpenseItem 
+              title={props.expenses[0].title} 
+              date={props.expenses[0].date}
+              amount={props.expenses[0].amount}
+              />
+      </Card>
+    </div>
   )
 }
 
