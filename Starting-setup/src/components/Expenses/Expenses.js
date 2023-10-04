713794d6cb31afe7091588ed126ce81140ca0ac3
daps10 <darpanchavhan75@gmail.com>
2023-10-04 18:35:05 +0530

    Feat: added expensefilter inside the card.

--- Starting-setup/src/components/Expenses/Expenses.js
+++ Starting-setup/src/components/Expenses/Expenses.js
@@ -6,15 +6,16 @@ import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
-  
+
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
   }
 
   return (
     <div>
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
       <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+        <p>Data for years {filterInfoText} is hidden.</p>
         <ExpenseItem 
               title={props.expenses[0].title} 
               date={props.expenses[0].date}
