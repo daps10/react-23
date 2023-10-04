abd95c776cc8b3cae5ef019ea1e569de6ce5a964
daps10 <darpanchavhan75@gmail.com>
2023-10-04 19:51:29 +0530

    Feat: Rendering of list data
    - Instead of static listing we've added javascript's map function .
    - Map function which would be handling the array values in the single objects.

--- Starting-setup/src/components/Expenses/Expenses.js
+++ Starting-setup/src/components/Expenses/Expenses.js
@@ -16,11 +16,21 @@ const Expenses = (props) => {
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
         <p>Data for years {filterInfoText} is hidden.</p>
+        {
+          props.expenses.map((expense) => <ExpenseItem title={expense.title} 
+          date={expense.date}
+          amount={expense.amount} />)
+        };
+
+          
+        {/* 
+        Removed the static method which consider to add multiple objects
+        
         <ExpenseItem 
               title={props.expenses[0].title} 
               date={props.expenses[0].date}
               amount={props.expenses[0].amount}
-              />
+              /> */}
       </Card>
     </div>
   )
