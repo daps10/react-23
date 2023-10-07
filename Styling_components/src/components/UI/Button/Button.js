d14792100a97bf6648704a4221cf13f9fbf68520
daps10 <darpanchavhan75@gmail.com>
2023-10-07 23:51:45 +0530

    Feat: Added css module
    - For adding CSS module we've changed the css file with button.module.css
    - Now that imported CSS we can take it as module's object.

--- Styling_components/src/components/UI/Button/Button.js
+++ Styling_components/src/components/UI/Button/Button.js
@@ -1,7 +1,6 @@
-// import React from 'react';
-import styled from 'styled-components';
-// import './Button.css';
-
+import React from 'react';
+// import styled from 'styled-components';
+import styles from './Button.module.css';
 
 // styled components
 // const Button = styled.button`
@@ -30,12 +29,12 @@ import styled from 'styled-components';
 // }
 // `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
