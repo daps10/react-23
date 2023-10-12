1beeb24deb00d8273465b7047c1d34c214983bab
daps10 <darpanchavhan75@gmail.com>
2023-10-12 23:37:17 +0530

    Feat: handling user input

--- Demo_project/src/components/User/AddUser.js
+++ Demo_project/src/components/User/AddUser.js
@@ -10,6 +10,17 @@ const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
     console.log(enteredUsername, enteredAge);
+    
+    if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+      return;
+    }
+
+    if(+enteredAge < 1){
+      return;
+    }
+
+    setEnteredAge('');
+    setEnteredUsername('');
   };
 
   const usernameChangeHandler = (event) => {
@@ -24,10 +35,20 @@ const AddUser = () => {
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler}/>
+        <input 
+          id="username" 
+          type="text" 
+          value={enteredUsername}
+          onChange={usernameChangeHandler}
+          />
 
         <label htmlFor="age"> Age (Years) </label>
-        <input id="age" type="number" onChange={ageChangeHandler}/>
+        <input 
+          id="age" 
+          type="number" 
+          value={enteredAge}
+          onChange={ageChangeHandler}
+          />
 
         <Button type="submit"> Add User </Button>
         {/* <button type="submit">Add User</button> */}
