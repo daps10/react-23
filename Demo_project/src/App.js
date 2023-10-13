fdedb7a8d080e3e2fb57f6f005e139ddf6f7a727
daps10 <darpanchavhan75@gmail.com>
2023-10-13 18:09:31 +0530

    Feat: Adding userlist component.

--- Demo_project/src/App.js
+++ Demo_project/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddUser from './components/User/AddUser';
+import UserList from './components/User/UserList';
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UserList users={[]}/>
     </div>
   );
 }
