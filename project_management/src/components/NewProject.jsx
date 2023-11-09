094f98fe2115a7c568e624c55d88fff9290200d1
daps10 <darpanchavhan75@gmail.com>
2023-11-09 18:29:06 +0530

    Feat: Validating user inputs and showing error modal
    - Created new component for modal which would help to display modal if we passed wrong inputs.

--- project_management/src/components/NewProject.jsx
+++ project_management/src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 export default function NewProject({onAdd}) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -12,6 +15,15 @@ export default function NewProject({onAdd}) {
     const enteredDueDate = dueDate.current.value;
 
     // validations...
+    if(
+      enteredTitle.trim() === '' || 
+      enteredDescription.trim() === '' ||
+      enteredDueDate.trim() || ''
+    ) {
+      // show error model
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -20,22 +32,32 @@ export default function NewProject({onAdd}) {
     });
   }
 
-  return <div className="w-[35rem] mt-16">
-    <menu className="flex item-center justify-end gap-4 my-4">
-      <li>
-        <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-      </li>
-      <li>
-        <button 
-          className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          onClick={handleSave}
-          >Save</button>
-      </li>
-    </menu>
-    <div>
-      <Input type='text' ref={title} label="Title"/>
-      <Input ref={description} label="Description" textarea/>
-      <Input type='date' ref={dueDate} label="Due Date" />
-    </div>
-  </div>
+  return (
+    <>
+      <Modal ref={modal} buttonCaption='Okay'> 
+        <h2>Invalid Input</h2>
+        <p>Ooops... looks like you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+
+      <div className="w-[35rem] mt-16">
+        <menu className="flex item-center justify-end gap-4 my-4">
+          <li>
+            <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+          </li>
+          <li>
+            <button 
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+              >Save</button>
+          </li>
+        </menu>
+        <div>
+          <Input type='text' ref={title} label="Title"/>
+          <Input ref={description} label="Description" textarea/>
+          <Input type='date' ref={dueDate} label="Due Date" />
+        </div>
+      </div>
+    </>
+  );
 }
