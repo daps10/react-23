18f2343c21029425563fecf1571043b94fb9400d
daps10 <darpanchavhan75@gmail.com>
2023-11-10 15:42:04 +0530

    Feat: Managing tasks
    - Used prop drilling for tasks.
    - To handle add task component added function inside the app component that will drilled till the newTask component.
    - Same as deleteTask function would be drill till selected project.

--- project_management/src/App.jsx
+++ project_management/src/App.jsx
@@ -7,8 +7,34 @@ import SelectedProject from "./components/SelectedProject";
 function App() {
   const [projectState, setProjectState] = useState({
     selectedProjectID: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   });
+
+  // handle Add task
+  function handleAddTask(textTask) {
+    setProjectState((prevState) => {
+      const taskId = Math.random();
+      const newTask = {
+        text:textTask,
+        projectId: prevState.selectedProjectID,
+        id: taskId,
+      };
+
+      return {
+        ...prevState,
+        tasks: [
+          newTask,
+          ...prevState.tasks 
+        ]
+      };
+    });
+  }
+
+  // handle delete task
+  function handleDeleteTask() {
+    
+  }
 
   // handle start project 
   function handleStartAddProject() {
@@ -29,7 +55,6 @@ function App() {
       }
     });
   }
-  
 
   // handlerAddProject
   function handleAddProject(projectData) {
@@ -78,14 +103,21 @@ function App() {
   const selectedProject= projectState.projects.find(project => project.id === projectState.selectedProjectID);
 
   // content showcase
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+  let content = (
+    <SelectedProject 
+      project={selectedProject} 
+      onDelete={handleDeleteProject} 
+      tasks= {projectState.tasks}
+      onAddTask={handleAddTask} 
+      onDeleteTask={handleDeleteTask}
+    />
+  );
   if(projectState.selectedProjectID === null) {
     content = (<NewProject onAdd={ handleAddProject } onCancel={handleCancelAddProject}/>);
   } else if(projectState.selectedProjectID === undefined) {
     content = (<NoProjectSelected onStartAddProject={handleStartAddProject} />);
   }
 
-  
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar 
--- project_management/src/components/SelectedProject.jsx
+++ project_management/src/components/SelectedProject.jsx
@@ -1,6 +1,12 @@
 import Tasks from "./Tasks"
 
-export default function SelectedProject({project, onDelete}) {
+export default function SelectedProject({
+  project, 
+  onDelete,
+  onAddTask,
+  onDeleteTask,
+  tasks
+}) {
   const formattedDate = new Date(project.dueDate).toLocaleDateString(
     'en-US',
     {
@@ -24,7 +30,11 @@ export default function SelectedProject({project, onDelete}) {
         <p className="mb-4 text-stone-400"> {formattedDate} </p>
         <p className="text-stone-600 whitespace-pre-line"> {project.description} </p>
       </header>
-      <Tasks />
+      <Tasks 
+        onAdd={onAddTask} 
+        onDelete={onDeleteTask}
+        tasks={tasks}
+      />
     </div>
   )
 }
