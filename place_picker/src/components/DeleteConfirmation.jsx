2ae357a4cfa646a43aed6a4dea8e96cb31991066
daps10 <darpanchavhan75@gmail.com>
2023-11-11 22:53:53 +0530

    Feat: Used setTimerout timer to delete place automatically

--- place_picker/src/components/DeleteConfirmation.jsx
+++ place_picker/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,10 @@
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  // timer function to confirm automatically
+  setTimeout(() => {
+    onConfirm();
+  }, 3000);
+
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
