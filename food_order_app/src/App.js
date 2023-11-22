c2e31ca0e07a3991d44feed2d1775c3d0fbd7eed
daps10 <darpanchavhan75@gmail.com>
2023-11-22 21:59:28 +0530

    [Feat]: managing cart and modal state
    - In this we managed the state for cartIsShown or not.
    - With the help of useState hook we passed it to the cart component and header component.

--- food_order_app/src/App.js
+++ food_order_app/src/App.js
@@ -1,13 +1,27 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  // to manage cart is visible or not 
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  // cart show
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  // cart hide
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      { cartIsShown && <Cart onClose={ hideCartHandler }/>}
+      
+      <Header onShowCart={ showCartHandler }/>
       <main>
         <Meals />
       </main>
