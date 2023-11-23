431be4e09c2763a3276368759c91b4f31dd73ece
daps10 <darpanchavhan75@gmail.com>
2023-11-23 22:47:14 +0530

    [Feat]: Understanding useMemo hook
    - This hook allow the function will called only once and whenever it's dependency changed that time it will re-render.

--- counterApp/src/components/Counter/Counter.jsx
+++ counterApp/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState, memo, useCallback } from 'react';
+import { useState, memo, useCallback, useMemo } from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -30,7 +30,8 @@ function isPrime(number) {
 // used memo
 const Counter = memo (function Counter({ initialCount }) {
   log('<Counter /> rendered', 1);
-  const initialCountIsPrime = isPrime(initialCount);
+  // used useMemo 
+  const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
   const [counter, setCounter] = useState(initialCount);
 
