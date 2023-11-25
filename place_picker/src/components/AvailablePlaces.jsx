0e8dc8abc90a70ebc9832d1a239b008dde60efcd
daps10 <darpanchavhan75@gmail.com>
2023-11-25 23:10:45 +0530

    [Feat]: fetch api with fetch method.
    - Retrieve data from the backend API we used the fetch method.
    - Fetch method generally used to retrieve data from the api as promises format.

--- place_picker/src/components/AvailablePlaces.jsx
+++ place_picker/src/components/AvailablePlaces.jsx
@@ -6,7 +6,15 @@ const places = localStorage.getItem('places');
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  
+  // fetching data with fetch API
+  // it returns promises to handle with then method
+  fetch('http://localhost:3000/places')
+    .then(( response ) => {
+      return response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places);
+    });
   
   return (
     <Places
