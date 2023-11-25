6584a000e61c368ff57aa52ef0111458a70e54d1
daps10 <darpanchavhan75@gmail.com>
2023-11-25 23:15:50 +0530

    [Feat]: Sending HTTP requests via useEffect
    - With the help of useEffect we called the api.
    - which simply allow to be use the api call only once instead of called it multiple times.

--- place_picker/src/components/AvailablePlaces.jsx
+++ place_picker/src/components/AvailablePlaces.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
 
 const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  
+  useEffect(() => {
+    // fetching data with fetch API
+    // it returns promises to handle with then method
+    fetch('http://localhost:3000/places')
+      .then(( response ) => {
+        return response.json();
+      })
+      .then((resData) => {
+        setAvailablePlaces(resData.places);
+      });
+  }, [])
 
-  // fetching data with fetch API
-  // it returns promises to handle with then method
-  fetch('http://localhost:3000/places')
-    .then(( response ) => {
-      return response.json();
-    })
-    .then((resData) => {
-      setAvailablePlaces(resData.places);
-    });
   
   return (
     <Places
