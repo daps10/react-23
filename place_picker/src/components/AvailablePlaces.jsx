f87b7e298fdea9d7670644b4c132e764cf62fb07
daps10 <darpanchavhan75@gmail.com>
2023-11-25 23:54:08 +0530

    [Feat]: handling HTTP errors
    - in this we checked the error occurs or not during api calling.
    - If we got an error we should call the error module which would return the error component in the response.

--- place_picker/src/components/AvailablePlaces.jsx
+++ place_picker/src/components/AvailablePlaces.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react';
 import Places from './Places.jsx';
+import Error from './Error.jsx';
 
 const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
     // initialized async await
@@ -13,12 +15,24 @@ export default function AvailablePlaces({ onSelectPlace }) {
       // to indicated that we are fetching data so show the loading text
       setIsFetching(true);  
 
-      // fetching data with fetch API
-      // it returns promises to handle with then method
-      const response= await fetch('http://localhost:3000/places');
-      const resData= await response.json();
+      try {
+        // fetching data with fetch API
+        // it returns promises to handle with then method
+        const response= await fetch('http://localhost:3000/places');
+        const resData= await response.json();
+        
+        // check response is 200 or not
+        if(!response.ok) {
+          throw new Error('Failed to fetch places');
+        }
 
-      setAvailablePlaces(resData.places);
+        // set places in available places state.
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({ 
+          message: error.message || 'Could not fetch places, please try agin later!' 
+        });      
+      }
 
       // to indicated that we are fetching data so hide the loading text
       setIsFetching(false);
@@ -27,7 +41,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
     fetchPlaces();
   }, [])
 
-  
+  if(error) {
+    return <Error 
+      title= "An error occurred!"
+      message= { error.message }
+    />
+  }
+
   return (
     <Places
       title="Available Places"
