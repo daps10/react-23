c78819b8febc628e5f46dbd2719ff2c20ad41b14
daps10 <darpanchavhan75@gmail.com>
2023-11-25 23:40:22 +0530

    [Feat]: Handling loading states
    - In this we handled the loading states with help of the useState.
    - We passed the isfetching state inside the places with true or false.
    - Once the data has been fetched it would be changed

--- place_picker/src/components/AvailablePlaces.jsx
+++ place_picker/src/components/AvailablePlaces.jsx
@@ -5,16 +5,23 @@ const places = localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
-  
+  const [isFetching, setIsFetching] = useState(false);
+
   useEffect(() => {
     // initialized async await
     async function fetchPlaces() {
+      // to indicated that we are fetching data so show the loading text
+      setIsFetching(true);  
+
       // fetching data with fetch API
       // it returns promises to handle with then method
       const response= await fetch('http://localhost:3000/places');
       const resData= await response.json();
 
       setAvailablePlaces(resData.places);
+
+      // to indicated that we are fetching data so hide the loading text
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -25,6 +32,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={ availablePlaces }
+      isLoading={ isFetching }
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
