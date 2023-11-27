338e3b4fe3c9e8e9f67113c95269405d36c1330a
daps10 <darpanchavhan75@gmail.com>
2023-11-27 18:40:22 +0530

    [Feat]: using a custom hook in multiple component
    - Inside the availablePlaces component we used the useFetch component which would simply used same as the app.js file.

--- place_picker/src/components/AvailablePlaces.jsx
+++ place_picker/src/components/AvailablePlaces.jsx
@@ -3,50 +3,36 @@ import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import useFetch from '../hooks/useFetch.js';
 
-const places = localStorage.getItem('places');
+// const places = localStorage.getItem('places');
+
+// get users location
+// navigator.geolocation.getCurrentPosition((position) => {
+//   // sorted places with the got places from API,
+//   // with current lat and long
+//   const sortedPlaces = sortPlacesByDistance(
+//     places, 
+//     position.coords.latitude, 
+//     position.coords.longitude
+//   );
+  
+//   // set places in available places state.
+//   setAvailablePlaces(sortedPlaces);
+  
+//   // to indicated that we are fetching data so hide the loading text
+//   setIsFetching(false);
+// });
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
+  
+  const {
+    isFetching, 
+    error, 
+    fetchedData: availablePlaces, 
+    setFetchedData: setAvailablePlaces
+   } = useFetch(fetchAvailablePlaces, []);
 
-  useEffect(() => {
-    // initialized async await
-    async function fetchPlaces() {
-      // to indicated that we are fetching data so show the loading text
-      setIsFetching(true);  
-
-      try {
-        // called the fecth available places function
-        const places= await fetchAvailablePlaces();
-
-        // get users location
-        navigator.geolocation.getCurrentPosition((position) => {
-          // sorted places with the got places from API,
-          // with current lat and long
-          const sortedPlaces = sortPlacesByDistance(
-            places, 
-            position.coords.latitude, 
-            position.coords.longitude
-          );
-          
-          // set places in available places state.
-          setAvailablePlaces(sortedPlaces);
-          
-          // to indicated that we are fetching data so hide the loading text
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError({ 
-          message: error.message || 'Could not fetch places, please try agin later!' 
-        });      
-        setIsFetching(false);
-      }
-    }
-
-    fetchPlaces();
-  }, [])
 
   if(error) {
     return <Error 
