baf0bcfc55b9f3771f1959a57d0c9939b2dba90b
daps10 <darpanchavhan75@gmail.com>
2023-11-29 15:11:23 +0530

    [Feat]: resetting forms
    - Reset forms with the state, refs and inbuilt-method.

--- react-forms/src/components/StateLogin.jsx
+++ react-forms/src/components/StateLogin.jsx
@@ -16,6 +16,11 @@ export default function Login() {
     e.preventDefault();
 
     console.log('User email :: ', enteredValues);
+
+    setEnteredValues({
+      email: '',
+      password: ''
+    })
   }
 
   function handleInputChange(identifire, value) { 
