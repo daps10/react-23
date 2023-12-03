009e883f7071c9833f2c78357b2e714c47d5b649
daps10 <darpanchavhan75@gmail.com>
2023-12-03 15:47:27 +0530

    [Feat]: Providing the store
    - Here we imported Provider from react-dom/client and store to provide the store in highest level.

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import store from '../store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+// providing provider to app
+root.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>
+);
