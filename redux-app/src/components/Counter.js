099b2737d44d6ead61b380d26ee8355a37428c8b
daps10 <darpanchavhan75@gmail.com>
2023-12-03 16:16:20 +0530

    [Feat]: dispatching actions from inside component
    - In this we imported dispatch hook which would allow us to send an actions to the redux store.
    - With useDispatch hook we send the actions to the store and that would do the action

--- redux-app/src/components/Counter.js
+++ redux-app/src/components/Counter.js
@@ -1,9 +1,24 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
   // to used to get selected state from the store.
   const counter= useSelector(state => state.counter);
+
+  // dispatch return function
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({
+      type: 'increment'
+    })
+  }
+
+  const decrementHandler = () => {
+    dispatch({
+      type: 'decrement'
+    })
+  }
 
   const toggleCounterHandler = () => {};
 
@@ -11,6 +26,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{ counter }</div>
+      <div>
+        <button onClick={ incrementHandler }>Increment</button>
+        <button onClick={ decrementHandler }>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
