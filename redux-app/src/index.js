50a59e9df302f56d4474ef5963b29eff7132f7cd
daps10 <darpanchavhan75@gmail.com>
2023-12-03 15:56:49 +0530

    [Fixed]: fixed some jargons which we got inside inside.js file.

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-dom/client';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
-import store from '../store';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 // providing provider to app
-root.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>
-);
+root.render(<Provider store={ store }><App /></Provider>);
