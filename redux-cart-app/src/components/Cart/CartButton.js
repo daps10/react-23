eed4f04a942b8d9e3b43e8e9659b445a60644105
daps10 <darpanchavhan75@gmail.com>
2023-12-04 21:30:44 +0530

    [Feat]: used to toggle action.
    - toggled the cart we used the action from uiActions.
    - And in app.js file we get the states from store using the uiSlice.

--- redux-cart-app/src/components/Cart/CartButton.js
+++ redux-cart-app/src/components/Cart/CartButton.js
@@ -1,8 +1,19 @@
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 import classes from './CartButton.module.css';
 
 const CartButton = (props) => {
+  // dispatch an action
+  const dispatch= useDispatch();
+  
+  // toggle cart handler
+  const toggleCartHandler= () => {
+    // dispatch the UIactions
+    dispatch(uiActions.toggle());
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={ toggleCartHandler }>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
