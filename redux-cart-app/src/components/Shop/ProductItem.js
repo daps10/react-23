c8f8f763496f457f784c453789ace132f4cb91a0
daps10 <darpanchavhan75@gmail.com>
2023-12-10 15:48:24 +0530

    [Feat]: added new replace slice.
    - This slice allow us to integrate the replace of items which is inside the array.

--- redux-cart-app/src/components/Shop/ProductItem.js
+++ redux-cart-app/src/components/Shop/ProductItem.js
@@ -1,4 +1,4 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 import { cartActions } from '../../store/cart-slice';
@@ -6,17 +6,60 @@ import { cartActions } from '../../store/cart-slice';
 const ProductItem = (props) => {
   const {  title, price, description, id } = props;
   
+  // useSelector to get cart 
+  // const cart= useSelector((state) => state.cart);
+
   // useDispatch to dispatched the reducers functions
   const dispatch= useDispatch(); 
 
   // addtocart handler
   const addToCartHandler = () => {
-    dispatch(cartActions.addItemToCart({
-      id,
-      title,
-      price,
-      description
-    }));
+    // // totalNewQuantity 
+    // const newTotalQuantity = cart.totalQuantity + 1;
+
+    // const updatedItems= cart.items.slice(); // create copy via slice to avoid mutating
+    // const existingItem = updatedItems.find((item) => item.id === id);
+
+    // // check item exist or not
+    // if(existingItem) {
+    //   const updateItem = { ...existingItem };
+    //   // update quantity and total price
+    //   updateItem.quantity++;
+    //   updateItem.totalPrice= updateItem.totalPrice + price;
+    //   // check index of existing item
+    //   const existingItemIndex= updatedItems.findIndex((item) => item.id === id);
+    //   // Then pushed update item inside it
+    //   updatedItems[existingItemIndex] = updateItem;
+    // } else {
+    //   // If not exist then push it into updatedItems array
+    //   updatedItems.push({
+    //     id: id,
+    //     price: price,
+    //     quantity: 1,
+    //     totalPrice: price,
+    //     name: title,
+    //     description: description
+    //   });
+    // }
+
+    // // new cart for replace the existing cart.
+    // const newCart= {
+    //   totalQuantity: newTotalQuantity,
+    //   items: updatedItems
+    // }
+
+    // dispatch(cartActions.replaceCart(newCart));
+
+
+    // with addItem
+    // dispatch(cartActions.addItemToCart({
+    //   id,
+    //   title,
+    //   price,
+    //   description
+    // }));
+  
+  
   }
 
   return (
