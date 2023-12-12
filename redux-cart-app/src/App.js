ed4f19b8e301aa0472b49942fa36566e2b51407b
daps10 <darpanchavhan75@gmail.com>
2023-12-12 22:13:14 +0530

    [Feat]: created thunk function
    - Created thunk function which is simply worked as the calling the redux functionality inside the slice.
    - Then we can just called that thunk function inside the app.js to perform the side effect functionality.

--- redux-cart-app/src/App.js
+++ redux-cart-app/src/App.js
@@ -3,8 +3,8 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { Fragment, useEffect } from 'react';
-import { uiActions } from './store/ui-slice';
 import Notification from './components/UI/Notification';
+import { sendCartData } from './store/cart-slice';
 
 let isInitial= true;
 
@@ -16,53 +16,13 @@ function App() {
 
   // useEffect
   useEffect(() => {
-    // called an function for await
-    const sendCart= async() => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending',
-        message: 'Sending cart data..'
-      }));
-
-      // called an API
-      const response= await fetch(
-        'https://react-movies-app-6b7ea-default-rtdb.firebaseio.com/cart-reduce.json', 
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart)
-        }
-      );
-
-      // check if response we havent got
-      if(!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      // dispatch success action
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success!',
-        message: 'Sent cart data successfully!'
-      }));
-
-      // set reponse in the responsedata
-      // const responseData= await response.json();
-    }
-
     if(isInitial) {
       isInitial= false;
       return;
     }
 
-    // sendcart catch if we found any error
-    sendCart().catch((error) => {
-      // dispatch success action
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed!'
-      }));
-    });
+    // called the thunk for asynchronous function
+    dispatch(sendCartData(cart));
 
   }, [cart, dispatch]);
   
--- redux-cart-app/src/store/cart-slice.js
+++ redux-cart-app/src/store/cart-slice.js
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
-
+import { uiActions } from "./ui-slice";
 
 const cartSlice= createSlice({
   name: 'cart',
@@ -53,6 +52,51 @@ const cartSlice= createSlice({
     }
   }
 })
+
+// create thunk
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(uiActions.showNotification({
+      status: 'pending',
+      title: 'Sending',
+      message: 'Sending cart data..'
+    }));
+
+    const sendRequest= async() => {
+      // called an API
+      const response= await fetch(
+        'https://react-movies-app-6b7ea-default-rtdb.firebaseio.com/cart-reduce.json', 
+        {
+          method: 'PUT',
+          body: JSON.stringify(cart)
+        }
+      );
+  
+      // check if response we havent got
+      if(!response.ok) {
+        throw new Error('Sending cart data failed.');
+      }
+    }
+
+    try {
+      await sendRequest();
+      
+      // dispatch success action
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success!',
+        message: 'Sent cart data successfully!'
+      }));
+    } catch (error) {
+      // dispatch success action
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error!',
+        message: 'Sending cart data failed!'
+      }));
+    }
+  }
+}
 
 export const cartActions= cartSlice.actions;
 export default cartSlice;
