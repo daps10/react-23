e90cee33134a06569015a80cde15e690330e46a2
daps10 <darpanchavhan75@gmail.com>
2023-12-14 17:13:51 +0530

    [Feat]: Showing error pages with error element
    - In this we created error component.
    - Which would allow to the showing error while we entered wrong url.
    - Now to use that we need to give the errorElement in the browser router.

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -4,15 +4,18 @@ import {
   createBrowserRouter, 
   createRoutesFromElements 
 } from 'react-router-dom';
+
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Root from './pages/Root';
+import Error from './pages/Error';
 
 // create browser router
 const router= createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Home /> },
       { path: '/products', element: <Products /> }
