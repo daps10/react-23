fb0d57da0c4b18214ff9ba6d6fc2701df94b8448
daps10 <darpanchavhan75@gmail.com>
2023-12-14 15:08:29 +0530

    [Feat]: exploring alternate ways to defining routes

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -1,14 +1,27 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { 
+  Route,
+  RouterProvider, 
+  createBrowserRouter, 
+  createRoutesFromElements 
+} from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 
 // create browser router
-const router= createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/products', element: <Products /> }
-]);
+// const router= createBrowserRouter([
+//   { path: '/', element: <Home /> },
+//   { path: '/products', element: <Products /> }
+// ]);
 
+// alternate ways to display router
+const routeDefinitions= createRoutesFromElements(
+  <Route>
+    <Route path='/' element={<Home />}  />
+    <Route path='/products' element={<Products />}  />
+  </Route>
+)
 
+const router= createBrowserRouter(routeDefinitions);
 function App() {
   return <RouterProvider router={ router }/>;
 }
