17254f0e188c886a14475f15b93daf1a5cce0bdb
daps10 <darpanchavhan75@gmail.com>
2023-12-14 23:08:08 +0530

    [Feat]: Fetched events and displayed.
    - fetched all events with dummy events array,
    - Put them inside the eventDetails and displayed the event id with useParams hook.

--- event-mgmt-app/frontend/src/pages/EventDetail.js
+++ event-mgmt-app/frontend/src/pages/EventDetail.js
@@ -1,6 +1,14 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
 function EventDetail(){
+  const params= useParams();
+
   return (
-    <h1>EventDetail page</h1>
+    <React.Fragment>
+      <h1>EventDetail page</h1>
+      <p>Event ID: {params.eventId }</p>
+    </React.Fragment>
   )
 }
 
--- event-mgmt-app/frontend/src/pages/Events.js
+++ event-mgmt-app/frontend/src/pages/Events.js
@@ -1,6 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const DUMMY_EVENTS = [
+  {
+    id: 'e1',
+    title: 'Some event',
+  },
+  {
+    id: 'e2',
+    title: 'Another event',
+  }
+]
 function Events(){
   return (
-    <h1>Events page</h1>
+    <React.Fragment>
+      <h1>Events page</h1>
+      <ul>
+        {
+          DUMMY_EVENTS.map((event) => (
+            <li key={event.id}>
+              <Link to={event.id}>{event.title}</Link>
+            </li>
+          ))
+        }
+      </ul>
+    </React.Fragment>
   )
 }
 
