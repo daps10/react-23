4ac19063e34016f1d0e48d311031ea1d36578785
daps10 <darpanchavhan75@gmail.com>
2023-12-17 13:31:27 +0530

    [Feat]: updating UI based on submitting status.
    - We used the useNavigation hook which allow us to given the current status and that should be used to update while we clicked on the save button.

--- event-mgmt-app/frontend/src/components/EventForm.js
+++ event-mgmt-app/frontend/src/components/EventForm.js
@@ -1,9 +1,15 @@
-import { Form, useNavigate } from 'react-router-dom';
+import { Form, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   const navigate = useNavigate();
+
+  // to used to check the status of transition
+  const navigation= useNavigation();
+
+  const isSubmitting= navigation.state === 'submitting';
+
   function cancelHandler() {
     navigate('..');
   }
@@ -51,10 +57,10 @@ function EventForm({ method, event }) {
         />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button type='submit'>Save</button>
+        <button type='submit' disabled={isSubmitting}>{ isSubmitting ? 'Submitting...' : 'Save'}</button>
       </div>
     </Form>
   );
