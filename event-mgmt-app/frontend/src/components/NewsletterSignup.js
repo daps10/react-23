66ab3d5c3f8aa7a904b0e28ecaa4510e5eb40a36
daps10 <darpanchavhan75@gmail.com>
2023-12-20 14:41:32 +0530

    [Feat]: behind the scenes working with useFetcher.
    - useFetcher() hook is used to work in behind scenes only so while we wanna submit a form without transitioning pages that time we can use the useFetcher hook

--- event-mgmt-app/frontend/src/components/NewsletterSignup.js
+++ event-mgmt-app/frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,30 @@
+import { useFetcher } from 'react-router-dom';
 import classes from './NewsletterSignup.module.css';
+import { useEffect } from 'react';
 
 function NewsletterSignup() {
+  const fetcher= useFetcher(); // this allow submit form without transitioning 
+  const { data, state } = fetcher;
+  
+  // useEffect to perform while the data or state changed.
+  useEffect(() => {
+    if(state=== 'idle' && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
+  
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form 
+      method="post" 
+      className={classes.newsletter}
+    >
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
