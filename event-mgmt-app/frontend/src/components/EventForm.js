d44b22b2ed3b215faa09da68a201df41203267a6
daps10 <darpanchavhan75@gmail.com>
2023-12-20 13:44:29 +0530

    [Feat]: Reusing actions via request methods.
    - In this we used the action inside the eventForm.
    - Where we declared the same code for save event inside the firebase.
    - With method we can use the different actions.

--- event-mgmt-app/frontend/src/components/EventForm.js
+++ event-mgmt-app/frontend/src/components/EventForm.js
@@ -1,5 +1,11 @@
-import { Form, useActionData, useNavigate, useNavigation } from 'react-router-dom';
-
+import { 
+  Form, 
+  useActionData, 
+  useNavigate, 
+  useNavigation,
+  json, 
+  redirect
+} from 'react-router-dom';
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
@@ -18,7 +24,7 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <Form method='post' className={classes.form}>
+    <Form method={method} className={classes.form}>
       { data && data.errors && (
         <ul>
           { Object.values(data.errors).map(err => <li key={err}>{err}</li> ) }
@@ -75,3 +81,42 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const method= request.method; // extract the method
+  
+  const data= await request.formData(); // formdata with request
+  
+  // get event data
+  const eventData= {
+    title: data.get('title'),
+    image: data.get('image'),
+    date: data.get('date'),
+    description: data.get('description'),
+  };
+
+  let URL= 'http://localhost:8080/events';
+  
+  if(method === 'PATCH') {
+    const eventId= params.eventId;
+    URL= `http://localhost:8080/events/${eventId}`;
+  }
+
+  const response= await fetch( URL, {
+    method: method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(eventData)
+  });
+  
+  if(response.status === 422){ 
+    return response;
+  }
+
+  if(!response.ok){
+    throw json({message: 'Could not save event.'}, { status: 500 })
+  }
+
+  return redirect('/events');
+}
