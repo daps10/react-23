acede5c1c6b2c44eb599e55ec67c85bebb5d2f51
daps10 <darpanchavhan75@gmail.com>
2024-01-08 19:09:24 +0530

    [Feat]: Updating UI based on auth status
    - In the app file added loader for token loader which can consider the authentication token.
    - That we used inside the several routes with the help of useRouteLoaderData.
    - That will return the token and that token through we added restriction on the add new event and edit and delete event button.

--- event-mgmt-app/frontend/src/components/EventItem.js
+++ event-mgmt-app/frontend/src/components/EventItem.js
@@ -1,7 +1,9 @@
-import { Link, useSubmit } from 'react-router-dom';
+import { Link, useRouteLoaderData, useSubmit } from 'react-router-dom';
 import classes from './EventItem.module.css';
 
 function EventItem({ event }) {
+  const token= useRouteLoaderData('root');
+  
   // to use the submit function
   const submit= useSubmit();
 
@@ -24,10 +26,12 @@ function EventItem({ event }) {
       <h1>{event.title}</h1>
       <time>{event.date}</time>
       <p>{event.description}</p>
-      <menu className={classes.actions}>
-        <Link to='edit'>Edit</Link>
-        <button onClick={startDeleteHandler}>Delete</button>
-      </menu>
+      { token && ( 
+        <menu className={classes.actions}>
+          <Link to='edit'>Edit</Link>
+          <button onClick={startDeleteHandler}>Delete</button>
+        </menu>
+      )}
     </article>
   );
 }
