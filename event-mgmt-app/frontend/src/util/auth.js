cc7540ba7a6b98c711d1312f650480d741dfd497
daps10 <darpanchavhan75@gmail.com>
2024-01-08 19:19:20 +0530

    [Feat]: adding route protection
    - added new function inside the auth which could checked the token exist or not.
    - If not then redirect the login page.
    - That function used inside the app file to manage the loader,

--- event-mgmt-app/frontend/src/util/auth.js
+++ event-mgmt-app/frontend/src/util/auth.js
@@ -1,8 +1,24 @@
+import { redirect } from "react-router-dom";
+
 export function getAuthToken() {
   const token= localStorage.getItem('token');
   return token;
 }
 
+// returns the auth token
 export function tokenLoader() {
   return getAuthToken();
+}
+
+// check authentication
+export function checkAuthLoader() {
+  // this function will be added in the next lecture
+  // make sure it looks like this in the end
+  const token= getAuthToken();
+
+  if(!token) {
+    return redirect('/auth');
+  }
+
+  return null;
 }
