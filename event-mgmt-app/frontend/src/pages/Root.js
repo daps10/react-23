940e1625c658509b20f4493c216b6f77180a6238
daps10 <darpanchavhan75@gmail.com>
2024-01-08 19:45:26 +0530

    [Feat]: Managing token expiration.
    - In auth file we created new function which will be returned the duration of token remaining.
    - that we used inside the authToken and returned the expired keyword.
    - In root file we used the useEffect which checked the token is available or not then checked the token has been expired or not and in setTimeout put the remaining duration from the getTokenDuration function.
    - During logout now also removed the expiration time.

--- event-mgmt-app/frontend/src/pages/Root.js
+++ event-mgmt-app/frontend/src/pages/Root.js
@@ -1,9 +1,32 @@
-import React from "react";
-import { Outlet, useNavigation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useLoaderData, useNavigation, useSubmit } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
+import { getTokenDuration } from "../util/auth";
 
 function Root(){
   // const navigation= useNavigation();
+  const token= useLoaderData();
+  const submit= useSubmit();
+
+  // check token exist or not
+  useEffect(() => {
+    // if not token exist
+    if(!token) {
+      return;
+    }
+
+    // check token expired or not
+    if(token === 'EXPIRED') {
+      submit(null, { action: '/logout', method: 'post' })
+      return;
+    }
+
+    const tokenDuration= getTokenDuration();
+    // called this after 1hr
+    setTimeout(() => {
+      submit(null, { action: '/logout', method: 'post' })
+    }, tokenDuration);
+  }, [token, submit]);
 
   return (
     <>
